d1547762bfc48d51a08a1779b6a8d4fb5d82f901
house227 <house1000227@gmail.com>
2022-01-09 19:40:23 +0900

    ユーザー登録/ログイン画面Topレイアウト 次回登録画面レイアウト

---
+++ src/resources/js/components/Login/Login_Top.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <body>
+      <a @click="$router.go(-1)" id="back">←前へ戻る</a>
+
+      <div class="mail-form">
+
+        <h2 style="margin-bottom:0px; padding-right:60px;">ご登録メールアドレス</h2>
+
+        <!-- 送信先/内容は要修正 -->
+        <form action="/logintop" >
+          <input type="email" name="mail" style="width:300px; height:25px; margin-top:0px;"> 
+          <h2 style="margin-bottom:0px; padding-right:180px;">パスワード</h2>
+          <input type="password" name="password" style="width:300px; height:25px; margin-top:0px;">
+          <br>
+          <input type="submit" value="ログイン" style="margin-top:20px; font-size:18px;">
+        </form>
+
+          <!-- パスワード再設定用 -->
+          <router-link to="/resetpass" style="padding-left:380px; color:blue;">
+            パスワードを忘れましたか？
+          </router-link>
+      </div>
+    </body>
+  </div>
+</template>
+
+<script>
+// import { reactive } from "vue";
+export default {
+  data(){
+    return{
+      error_cnt: 0,
+    }
+  },
+  mounted(){
+    
+  },
+
+};
+</script>
+
+<style>
+  #back{
+    cursor: hand;
+    cursor: pointer;
+    font-size: 25px;
+    font-weight: bold;
+    color: rgb(53, 53, 255);
+  }
+
+  .mail-form{
+    text-align: center;
+  }
+</style>
---
+++ src/resources/js/components/ResetPass/ResetPass_SendMail.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <body>
+      <router-link to="/" id="back">←Topへ戻る</router-link>
+
+      <!-- 送信先/内容は要修正 -->
+      <div class="mail-form">
+        <p>
+          ご入力頂いたメールアドレス宛てに <br> パスワード再設定用メールを送信致しました。<br>
+          受信メールBOX確認後記載されているURLから <br> パスワード再設定画面へアクセスして下さい。
+        </p>
+
+        <!-- 作成用リンク -->
+        <a href="">次へ</a>
+      </div>
+    </body>
+  </div>
+</template>
+
+<script>
+// import { reactive } from "vue";
+export default {
+  data(){
+    return{
+      message:'',
+    }
+  },
+  mounted(){
+    
+  }
+};
+</script>
+
+<style>
+  #back{
+    cursor: hand;
+    cursor: pointer;
+    font-size: 25px;
+    font-weight: bold;
+    color: rgb(53, 53, 255);
+  }
+
+  .mail-form{
+    text-align: center;
+  }
+</style>
