7be9b13939ac7cb2ff82ba32c0c41760417238ab
house227 <house1000227@gmail.com>
2022-01-12 16:05:50 +0900

    コメント欄作成途中 CSSを切り分け

--- src/resources/js/components/RecipeView/RecipeView_Guest.vue
+++ src/resources/js/components/RecipeView/RecipeView_Guest.vue
@@ -9,6 +9,8 @@
       <div id="recipe-top">
           <div id="recipe-img" style="padding-right:100px;">
             <!-- レシピ画像やお気に入り情報もDBから取りたいが今は仮配置とボタンでスイッチ -->
+            <!-- お気に入りテーブルにあるレシピ・ユーザーIDがセッション・現在のレシピと -->
+            <!-- 一致していればTrueにして♡の色を付ける -->
             <h2 @click="favoriteInput" id="favorite"> 
               <span style="color:red">
                 {{ favorite ? '♥' : '♡' }}
@@ -47,6 +49,40 @@
 
         <!-- コメント欄 -->
 
+        <h1>[コメント]</h1>
+
+        <!-- コメントテーブルからレシピIDが一致するコメントを取って来て表示させる -->
+        <!-- 下記コードをforで囲めば出来る？？ -->
+
+        <div id="comment-all">
+          <div class="comment">
+
+            <div class="comment-text">
+              <!-- ユーザーテーブルから一致する画像を持ってくる -->
+              <img src="photo/person.jpeg" alt="人" width="80" height="80">
+            </div>
+
+            <div class="comment-text">
+              <!-- ユーザーテーブルからコメントテーブルの外部キーと一致する名前を持ってくる -->
+              <p style="margin-top:0px; margin-bottom:0px">ユーザーネームA</p>
+              <!-- コメントテーブルからユーザーIDが一致するコメントを入れる -->
+              <p style="margin-left:15px; margin-bottom:0px; margin-top:10px; font-size:20px">コメントしました。</p>
+            </div>
+
+          </div>
+
+          <!-- コメント2つ目（テスト用 -->
+          <div class="comment">
+            <div class="comment-text">
+              <img src="photo/person.jpeg" alt="人" width="80" height="80">
+            </div>
+
+            <div class="comment-text">
+              <p>ユーザーネームA</p>
+              <p>コメントしました。</p>
+            </div>
+          </div>
+        </div>
 
     </body>
   </div>
@@ -154,5 +190,25 @@ export default {
     font-weight: bold;
     margin-bottom:25px;
     text-align: left;
+  }
+
+
+  /* コメント */
+
+
+  #comment-all{
+    
+  }
+
+  .comment{
+    display: flex;
+    border: 1px solid black;
+    width: 65%;
+    margin-bottom: 15px;
+    margin-left: 40px;
+  }
+  .comment-text{
+    display: inline-block;
+    flex-direction:column;
   }
 </style>
