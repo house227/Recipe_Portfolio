d21d4404c154729946beff68cb49bb1d3d86858d
house227 <house1000227@gmail.com>
2022-01-21 15:45:31 +0900

    api.phpにレシピ検索用のルート割り当て 次回検索部分を修正して検索できるようにする

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,16 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Laravel8から使用するコントローラを記述しなければならない
+use App\Http\Controllers\RecipeController;
+
+
+// Laravel8から書き方が変更された
+// APIに対応したRestfulにしておく
+// Restful：RESTに基いて設計され、情報の取得や送信など基本的な操作の方法が統一されているもの
+
+// recipesへ接続したら接続方法によってコントローラ内のアクションが割り振られる
+Route::apiResource('/recipes', RecipeController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
