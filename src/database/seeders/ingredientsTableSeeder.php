c60e97b1b8c544dfd9687ec9cf7829706687896d
house227 <house1000227@gmail.com>
2022-02-02 11:05:58 +0900

    なぜか反映されないシーダーを手動追加

--- src/database/seeders/ingredientsTableSeeder.php
+++ src/database/seeders/ingredientsTableSeeder.php
@@ -17,18 +17,34 @@ class ingredientsTableSeeder extends Seeder
         DB::table('ingredients')->insert([
 
             [
-                'recipes_id' => '1',
+                'recipe_id' => '1',
                 'content' => '鶏肉',
             ],
 
             [
-                'recipes_id' => '1',
+                'recipe_id' => '1',
                 'content' => '玉ねぎ',
             ],
 
             [
-                'recipes_id' => '1',
+                'recipe_id' => '1',
                 'content' => '卵',
+            ],
+            [
+                'recipe_id' => '2',
+                'content' => 'トマト',
+            ],
+            [
+                'recipe_id' => '2',
+                'content' => 'チーズ',
+            ],
+            [
+                'recipe_id' => '2',
+                'content' => 'ベーコン',
+            ],
+            [
+                'recipe_id' => '3',
+                'content' => '鶏肉',
             ],
         ]);
     }
--- src/database/seeders/recipesTableSeeder.php
+++ src/database/seeders/recipesTableSeeder.php
@@ -24,6 +24,18 @@ class recipesTableSeeder extends Seeder
                 'recipe_type' => '和食',
                 'photo' => 'https://park.ajinomoto.co.jp/wp-content/uploads/2018/03/708200.jpeg'
             ],
+            [
+                'appuser_id' => '2',
+                'title' => 'pizza',
+                'recipe_type' => 'イタリアン',
+                'photo' => 'https://partenope.jp/img/970x647/17.jpg'
+            ],
+            [
+                'appuser_id' => '2',
+                'title' => '唐揚げ',
+                'recipe_type' => '中華',
+                'photo' => 'https://www.kubara.co.jp/files/bgeditor/img/374__6Jya5YWI5oGV57i65Luj772Z.jpg'
+            ],
         ]);
     }
 }
